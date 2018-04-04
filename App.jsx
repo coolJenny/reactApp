fb8d0d4e6d72e1f98f55eacf9198aaff6f11888d
coolJenny <coolJenny009g@gmail.com>
2018-04-03 23:05:39 -0400

    ..

--- App.jsx
+++ App.jsx
@@ -4,7 +4,10 @@ class App extends React.Component {
    render() {
       return (
       	<div>         
-            Hello World!!!
+            <h1>Header</h1>
+            <h2>Content</h2>
+            <p data-myattribute = "somevalue">This is the content!!!</p>
+            <p>Math: 1+1 = {1+1}</p>
         </div>         
       );
    }
