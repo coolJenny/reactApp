ae70d7c819cbe2b666ac900385eab858e01e35a9
coolJenny <coolJenny009g@gmail.com>
2018-04-04 23:29:03 -0400

    ..

--- App.jsx
+++ App.jsx
@@ -2,21 +2,30 @@ import React from 'react';
 
 class App extends React.Component {
    render() {
-   		var i = 1;
-   		var myStyle = {
-   			font-size: 100,
-   			color: '#FF0000'
-   		}
       return (
-      	<div>         
-            <h1 style={myStyle}>Header</h1>
-            {//Single line comment...}
-          	{/*Multi line comment...*/}
+         <div>
+            <Header/>
+            <Content/>
+         </div>
+      );
+   }
+}
+class Header extends React.Component {
+   render() {
+      return (
+         <div>
+            <h1>Header</h1>
+         </div>
+      );
+   }
+}
+class Content extends React.Component {
+   render() {
+      return (
+         <div>
             <h2>Content</h2>
-            <p data-myattribute = "somevalue">This is the content!!!</p>
-            <p>Math: 1+1 = {1+1}</p>
-            <p>{i == 1 ? 'true!' : 'false!'}</p>
-        </div>
+            <p>The content text!!!</p>
+         </div>
       );
    }
 }
